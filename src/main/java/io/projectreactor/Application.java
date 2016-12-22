3a375c371010e33bd7440b9dec189ee22994c382
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-12-22 16:08:44 +0100

    Update for latest Spring Framework snapshot

--- src/main/java/io/projectreactor/Application.java
+++ src/main/java/io/projectreactor/Application.java
@@ -3,10 +3,10 @@ package io.projectreactor;
 import java.net.URI;
 
 import static org.springframework.http.HttpStatus.FOUND;
-import static org.springframework.web.reactive.function.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.RouterFunctions.route;
-import static org.springframework.web.reactive.function.RouterFunctions.resources;
-import static org.springframework.web.reactive.function.ServerResponse.status;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.status;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
 
@@ -14,16 +14,13 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
-import org.springframework.web.reactive.function.RouterFunction;
-import org.springframework.web.reactive.function.RouterFunctions;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.server.WebHandler;
-import org.springframework.web.server.adapter.HttpWebHandlerAdapter;
 import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * Main Application for the Project Reactor home site.
@@ -31,8 +28,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class Application {
 
 	public static void main(String... args) throws Exception {
-		// TODO See with Arjen if we can expose a method to return a WebHandler since internally web deal with a HttpWebHandlerAdapter
-		WebHandler webHandler = (HttpWebHandlerAdapter)RouterFunctions.toHttpHandler(routes());
+		WebHandler webHandler = RouterFunctions.toHttpHandler(routes());
 		HttpHandler httpHandler = WebHttpHandlerBuilder.webHandler(webHandler).filters(new IndexWebFilter()).build();
 
 		HttpServer.create("0.0.0.0")
@@ -64,8 +60,7 @@ public class Application {
 				status(FOUND).location(URI.create(request.path().replace("/ext/docs/", "/docs/test/release/"))).build())
 			.andRoute(GET("/ext/docs/api/**/adapter/**"), request ->
 				status(FOUND).location(URI.create(request.path().replace("/ext/docs/", "/docs/adapter/release/"))).build())
-			.and(resources("/**", new ClassPathResource("static/")))
-			;
+			.and(resources("/**", new ClassPathResource("static/")));
 	}
 
 	private static class IndexWebFilter implements WebFilter {
@@ -74,26 +69,10 @@ public class Application {
 		public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 			ServerHttpRequest request = exchange.getRequest();
 			return request.getURI().getPath().endsWith("/") ?
-				chain.filter(exchange.mutate().setRequest(new IndexServerHttpRequestDecorator(request)).build()) :
+				chain.filter(exchange.mutate().request(builder -> builder.path(request.getURI().getPath() + "index.html")).build()) :
 				chain.filter(exchange);
 		}
 
-		static class IndexServerHttpRequestDecorator extends ServerHttpRequestDecorator {
-
-			public IndexServerHttpRequestDecorator(ServerHttpRequest delegate) {
-				super(delegate);
-			}
-
-			@Override
-			public URI getURI() {
-				URI uri = super.getURI();
-				return UriComponentsBuilder.fromUri(uri).path("index.html").build().toUri();
-			}
-		}
-
-
-
 	}
-
 
 }
